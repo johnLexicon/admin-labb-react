0a4ebd89e81e49bba0a03ea3733b3967aa8e0144
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-19 15:41:59 +0100

    Added firebase config.

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyFVHH7GX6cTKxatMcBv6ONCY83odtLIU",
+    authDomain: "admin-labb-react.firebaseapp.com",
+    projectId: "admin-labb-react",
+    storageBucket: "admin-labb-react.appspot.com",
+    messagingSenderId: "441170868387",
+    appId: "1:441170868387:web:ffd1f484f76e372b26a716"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
