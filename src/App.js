5caa754244c042c70fcdeaecce917c9c4b37665d
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-20 06:58:48 +0100

    Added container.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import CreateUser from './views/CreateUser';
 import SignIn from './views/SignIn';
 import Home from './views/Home';
 import NavBar from './components/Navbar';
+import {Container} from '@material-ui/core';
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <Switch>
-        <Route path="/" exact><Home/></Route>
-        <Route path="/users" exact><Users/></Route>
-        <Route path="/create" exact><CreateUser/></Route>
-        <Route path="/signin" exact><SignIn/></Route>
-      </Switch>
+      <Container maxWidth="lg">
+        <Switch>
+          <Route path="/" exact><Home/></Route>
+          <Route path="/users" exact><Users/></Route>
+          <Route path="/create" exact><CreateUser/></Route>
+          <Route path="/signin" exact><SignIn/></Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
