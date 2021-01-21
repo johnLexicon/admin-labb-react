8a406e8b2c1397618aa0cb17d5c7fb07676779c2
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-21 15:54:20 +0100

    Added users actions.

---
+++ src/actions/usersActions.js
@@ -0,0 +1,28 @@
+import {actionTypes} from './actionTypes'
+import axios from 'axios'
+
+export const getUsersAction = () => {
+    return async dispatch => {
+        dispatch(loadingAction(true))
+        const response = await axios.get('http://localhost:9999/users')
+        dispatch(setUsersAction(response.data))
+        dispatch(loadingAction(false))
+    }
+}
+
+export const setUsersAction = (users) => {
+    const {SET_USERS} = actionTypes().users
+
+    return {
+        type: SET_USERS,
+        payload: {users}
+    }
+}
+
+export const loadingAction = (isLoading) => {
+    const {LOADING} = actionTypes().users
+    return {
+        type: LOADING,
+        payload: {isLoading}
+    }
+}
