8aff26a0f7d646d7bb119a8d4ab0d9a968551122
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-21 15:53:57 +0100

    Added action types for users.

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -3,6 +3,11 @@ export const actionTypes = () => {
     return {
         admin: {
             SET_ADMIN: 'SET_ADMIN'
+        },
+        users: {
+            GET_USERS: 'GET_USERS',
+            SET_USERS: 'SET_USERS',
+            LOADING: 'LOADING'
         }
     }
 }
