370dfb7e5d2a1bf2c0e2e9458839ebe3e0ceaa13
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-21 15:56:35 +0100

    Applied thunk middleware to store.

--- src/store.js
+++ src/store.js
@@ -1,4 +1,11 @@
-import {createStore} from 'redux'
-import {adminReducer} from './reducers/adminReducer';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 
-export default createStore(adminReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
