90ce07e7ff9efbe214053e4c3719343d050b4e93
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-22 15:44:41 +0100

    data fetcher for prod env.

---
+++ src/utils/prodRequests.js
@@ -0,0 +1,21 @@
+import {db} from '../firebase';
+
+const prodRequests = {
+    get: async () => {
+        const users = []
+        const snapshot = await db.collection('users').get()
+        snapshot.forEach(doc => {
+            users.push({id: doc.id, ...doc.data()})
+        })
+        return users
+    },
+    post: async (user) => {
+        const docRef = await db.collection('users').add(user)
+        return {id: docRef.id, ...user}
+    },
+    delete: async (userId) => {
+        await db.collection('users').doc(userId).delete()
+    }
+}
+
+export default prodRequests
