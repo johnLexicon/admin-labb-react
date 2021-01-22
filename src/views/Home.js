42d24aa3b57b5452f0f7a3cd221411147db035e8
john lundgren <john.lundgren.lexicon@gmail.com>
2021-01-22 19:43:37 +0100

    Added background image.

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,19 +3,18 @@ import { Box, Typography, makeStyles } from '@material-ui/core'
 
 const usingStyles = makeStyles(theme => ({
     hero: {
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1546177461-2d708782f5d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80')`,
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1567924675637-283a6742993e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80')`,
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         position: 'relative',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
         height: '100vh'
     },
     title:{
         fontSize: '5rem',
-        color: theme.palette.primary.light,
+        color: '#fff',
         [theme.breakpoints.down('xs')]: {
             fontSize: '3rem',
         }
@@ -26,7 +25,7 @@ const Home = () => {
     const classes = usingStyles()
     return (
         <Box className={classes.hero}>
-            <Typography className={classes.title}>React Blogger</Typography>
+            <Typography className={classes.title}>React Admin</Typography>
         </Box>
     )
 }
